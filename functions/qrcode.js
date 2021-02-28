fb290c54469d513988c7207635c0bec873feb8f6
akhnos <m.mertsolak@gmail.com>
2021-02-28 22:51:24 +0300

    get request fix

--- functions/qrcode.js
+++ functions/qrcode.js
@@ -3,11 +3,11 @@ import * as Bucket from "@spica-devkit/bucket";
 
 
 export default async function(req,res) {
-    const {qrID} = req.query.qr;
+    const qrID = req.query.qr;
     const qrCodeBucket = process.env.BUCKET_ID;
     Bucket.initialize({apikey: process.env.APIKEY});
 
     const qrData = await Bucket.data.get(qrCodeBucket, qrID);
     let code = await QRCode.toDataURL(qrData.data, {width: 300});
-    return res.status(200).send("<html><body><img src='" + code + "' /></body></html>");
+    return res.status(200).send({"image": code});
 }
